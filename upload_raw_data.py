fccac457cb1317b3de6fbdef0013a919e7f9b7d8
ayoxx <112950955+ayoxx@users.noreply.github.com>
2026-06-13 14:37:02 +0100

    feat: implement CSV upload to Azure Blob Storage

---
+++ upload_raw_data.py
@@ -0,0 +1,47 @@
+import os
+from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+location = Path(__file__).resolve().parent
+
+project_root = location
+
+load_dotenv()
+
+
+ACCOUNT_KEY=os.getenv('ACCOUNT_KEY')
+ACCOUNT_NAME = 'urbancitystorageayo'
+CONTAINER_NAME = 'bronze'
+
+LOCAL_FILE_PATH = project_root / "data" / "urban_service_requests.csv"
+
+BLOB_NAME = "urban_service_requests.csv"
+
+
+def upload_data():
+    try:
+        blob_service_client = BlobServiceClient(
+            account_url=f"https://{ACCOUNT_NAME}.blob.core.windows.net",
+            credential=ACCOUNT_KEY
+        )
+        
+        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=BLOB_NAME)
+        
+        with open(LOCAL_FILE_PATH, 'rb') as data:
+            blob_client.upload_blob(
+                data,
+                overwrite=True,
+                max_concurrency=4
+            )
+            
+            print(f"upload successful. File loaded to {CONTAINER_NAME}")
+        
+    except Exception as e:
+        print(e)
+    
+    return None
+
+
+upload_data()
